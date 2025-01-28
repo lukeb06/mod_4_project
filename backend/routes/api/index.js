6cb4247b07f9b510667eaa8e3a0cfa2bf1f21604
Luke Barrier <lukebbarrier06@outlook.com>
2025-01-28 09:51:24 -0500

    Update

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
 
-router.post('/test', function(req, res) {
+router.post('/test', function (req, res) {
     res.json({ requestBody: req.body });
-  });
-
-
+});
 
 module.exports = router;
+
