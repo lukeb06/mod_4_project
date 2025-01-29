b73b3fe7878311b29961f94c7b4b21d64e4d7758
Luke Barrier <lukebbarrier06@outlook.com>
2025-01-29 12:09:06 -0500

    Update

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -32,8 +32,6 @@ router.post('/', validateLogin, async (req, res, next) => {
         },
     });
 
-    console.log(user);
-
     if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
         const err = new Error('Login failed');
         err.status = 401;
