3b8f808582c4f84a840dc38f005748dc332f8998
Katie Ha <katie.ha98@gmail.com>
2025-01-28 22:59:44 -0500

    completed phase4 and phase5

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
-
-router.use(restoreUser);
-
 
 router.post('/test', function (req, res) {
     res.json({ requestBody: req.body });
@@ -12,38 +11,45 @@ router.post('/test', function (req, res) {
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 router.get('/set-token-cookie', async (_req, res) => {
-  const user = await User.findOne({
-    where: {
-      username: 'Demo-lition'
-    }
-  });
-  setTokenCookie(res, user);
-  return res.json({ user: user });
+    const user = await User.findOne({
+        where: {
+            username: 'Demo-lition',
+        },
+    });
+    setTokenCookie(res, user);
+    return res.json({ user: user });
 });
-
 
 // GET /api/restore-user
 
-router.get(
-  '/restore-user',
-  (req, res) => {
+router.get('/restore-user', (req, res) => {
     return res.json(req.user);
-  }
-);
-
-
-
+});
 
 // ...
 
 // GET /api/require-auth
 const { requireAuth } = require('../../utils/auth.js');
-router.get(
-  '/require-auth',
-  requireAuth,
-  (req, res) => {
+router.get('/require-auth', requireAuth, (req, res) => {
     return res.json(req.user);
-  }
-);
+});
+
+// Connect restoreUser middleware to the API router
+// If current user session is valid, set req.user to the user in the database
+// If current user session is not valid, set req.user to null
+
+router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+});
+
+router.post('/test', function (req, res) {
+    res.json({ requestBody: req.body });
+});
 
 module.exports = router;
