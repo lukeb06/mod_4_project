0920207fe43211f80f4a8702195cd71e15c94d18
Luke Barrier <lukebbarrier06@outlook.com>
2025-02-02 15:12:04 -0500

    Merged from staging

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,7 +2,7 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
-const spotsRouter = require('./spot.js');
+const spotsRouter = require('./spots.js');
 const { restoreUser } = require('../../utils/auth.js');
 
 // Connect restoreUser middleware to the API router
@@ -12,8 +12,6 @@ router.use(restoreUser);
 
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
-router.use('/spots', require('./spots'));
-
 router.use('/spots', spotsRouter);
 
 router.post('/test', function (req, res) {
