06d8411484856eb5e5293e6d557b2ed3673ee4dd
Luke Barrier <lukebbarrier06@outlook.com>
2025-02-04 10:37:17 -0500

    close: get all reviews of current user

---
+++ backend/routes/api/reviews.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+
+const { Review, Spot, ReviewImage, User, sequelize } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
+
+router.get('/current', requireAuth, async (req, res) => {
+    try {
+        const reviews = await Review.findAll({
+            where: {
+                userId: req.user.id,
+            },
+            include: [
+                {
+                    model: User,
+                    attributes: ['id', 'firstName', 'lastName'],
+                },
+                {
+                    model: Spot,
+                    attributes: {
+                        include: [[sequelize.fn('MAX', sequelize.col('url')), 'previewImage']],
+                        exclude: ['createdAt', 'updatedAt', 'description'],
+                    },
+                },
+                {
+                    model: ReviewImage,
+                    attributes: ['id', 'url'],
+                },
+            ],
+        });
+
+        return res.json(reviews);
+    } catch (error) {
+        res.status(400).json({
+            message: error.message,
+        });
+    }
+});
+
+module.exports = router;
