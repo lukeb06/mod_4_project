2bb1b0020c3ef98eab52c8516c9547ef4c30d1ae
Luke Barrier <lukebbarrier06@outlook.com>
2025-02-03 20:51:19 -0500

    fix: get all spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -89,6 +89,7 @@ router.get('/', async (_, res) => {
         const spots = await Spot.findAll({
             attributes: [
                 'id',
+                'ownerId',
                 'address',
                 'city',
                 'state',
@@ -137,12 +138,50 @@ router.get('/', async (_, res) => {
 router.get('/current', requireAuth, async (req, res) => {
     try {
         const spots = await Spot.findAll({
+            attributes: [
+                'id',
+                'ownerId',
+                'address',
+                'city',
+                'state',
+                'country',
+                'lat',
+                'lng',
+                'name',
+                'description',
+                'price',
+                'createdAt',
+                'updatedAt',
+                [sequelize.fn('MAX', sequelize.col('url')), 'previewImage'],
+                [sequelize.fn('AVG', sequelize.col('stars')), 'avgRating'],
+            ],
             where: {
                 ownerId: req.user.id,
             },
+            include: [
+                {
+                    model: SpotImage,
+                    attributes: ['url'],
+                    where: {
+                        preview: true,
+                    },
+                },
+                {
+                    model: Review,
+                    attributes: ['stars'],
+                },
+            ],
         });
 
-        return res.json(spots);
+        const spotsResponse = spots.map(spot => {
+            return {
+                ...spot.dataValues,
+                SpotImages: undefined,
+                Reviews: undefined,
+            };
+        });
+
+        return res.json(spotsResponse);
     } catch (error) {
         res.status(400).json({
             message: error.message,
