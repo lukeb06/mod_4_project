88c29ec39dab2527119e784b7807e2091822313b
Luke Barrier <lukebbarrier06@outlook.com>
2025-02-03 20:45:07 -0500

    get all spots owned by current user

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,41 +1,38 @@
 const express = require('express');
-const { check } = require('express-validator');
-const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
-const { handleValidationErrors } = require('../../utils/validation');
-const { booking } = require('../../db/models/booking');
-const booking = require('../../db/models/booking');
+const { requireAuth } = require('../../utils/auth');
 const router = express.Router();
+const { Booking } = require('../../db/models');
 
 // DELETE A BOOKING
 router.delete('/:bookingId', requireAuth, async (req, res, next) => {
     try {
-        const bookingId = req.params.id;
-        const bookingToDelete = await booking.findOne({
+        const { bookingId } = req.params;
+
+        const bookingToDelete = await Booking.findOne({
             where: {
                 id: bookingId,
             },
         });
-        const date = new Date();
 
-        if (bookingToDelete) {
-            if (req.user.id === booking.userId) {
-                await bookingToDelete.destroy();
-                res.status(200).json({
-                    message: 'Your booking has successfully been deleted',
-                });
-            }
-            if (date >= booking.startDate) {
-                res.status(400).json({
-                    message: 'Your booking has already been confirmed and cannot be deleted ',
-                });
-            }
+        if (!bookingToDelete) return res.status(404).json({ message: 'Booking not found' });
+        if (req.user.id !== bookingToDelete.userId)
+            return res.status(403).json({ message: 'Forbidden' });
+
+        if (date >= bookingToDelete.startDate) {
+            return res.status(400).json({
+                message: 'Your booking has already been confirmed and cannot be deleted ',
+            });
         }
+
+        await bookingToDelete.destroy();
+        return res.status(200).json({
+            message: 'Your booking has successfully been deleted',
+        });
     } catch (error) {
-        res.status(404).json({
-            message: 'booking was not found',
+        return res.status(404).json({
+            message: 'Booking was not found',
         });
     }
 });
 
 module.exports = router;
-
