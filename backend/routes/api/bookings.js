3a1f68b10458301a210e58a8c4489d3017e77740
Luke Barrier <lukebbarrier06@outlook.com>
2025-02-03 20:06:02 -0500

    add bookingsRouter

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -6,50 +6,36 @@ const { booking } = require('../../db/models/booking');
 const booking = require('../../db/models/booking');
 const router = express.Router();
 
-
-
-
-
-
 // DELETE A BOOKING
 router.delete('/:bookingId', requireAuth, async (req, res, next) => {
-   try {
-       const bookingId = req.params.id;
-       const bookingToDelete = await booking.findOne({
-           where: {
-              id: bookingId 
-           }
-       })
-       const date = new Date();
+    try {
+        const bookingId = req.params.id;
+        const bookingToDelete = await booking.findOne({
+            where: {
+                id: bookingId,
+            },
+        });
+        const date = new Date();
 
-
-       if (bookingToDelete) {
-           if (req.user.id === booking.userId) {
-               await bookingToDelete.destroy()
-               res.status(200).json({
-                   message: "Your booking has successfully been deleted"
-               })
-           } 
-           if (date >= booking.startDate) {
-               res.status(400).json({
-                   message: "Your booking has already been confirmed and cannot be deleted "
-               });
-           }
-           
-       }
-   } catch (error) {
-    res.status(404).json({
-        message: 'booking was not found',
-    });
-
-   }
+        if (bookingToDelete) {
+            if (req.user.id === booking.userId) {
+                await bookingToDelete.destroy();
+                res.status(200).json({
+                    message: 'Your booking has successfully been deleted',
+                });
+            }
+            if (date >= booking.startDate) {
+                res.status(400).json({
+                    message: 'Your booking has already been confirmed and cannot be deleted ',
+                });
+            }
+        }
+    } catch (error) {
+        res.status(404).json({
+            message: 'booking was not found',
+        });
+    }
 });
 
-
-
-
-
-
-
-
 module.exports = router;
+
