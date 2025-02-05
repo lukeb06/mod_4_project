bf44a9aceed9de325ecd66caf558269df74c2e01
Luke Barrier <lukebbarrier06@outlook.com>
2025-02-04 20:56:44 -0500

    Update

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -5,7 +5,6 @@ const { handleValidationErrors } = require('../../utils/validation');
 const { Booking, Spot, SpotImage, sequelize } = require('../../db/models');
 const router = express.Router();
 
-
 // DELETE A BOOKING
 router.delete('/:bookingId', requireAuth, async (req, res, next) => {
     try {
@@ -38,14 +37,12 @@ router.delete('/:bookingId', requireAuth, async (req, res, next) => {
     }
 });
 
-
 //  RETURN ALL OF THE BOOKINGS OF CURRENT USER
-
 router.get('/current', requireAuth, async (req, res, next) => {
     try {
         const currentBooking = await Booking.findAll({
             where: {
-                userId: req.user.id
+                userId: req.user.id,
             },
             include: [
                 {
@@ -54,22 +51,22 @@ router.get('/current', requireAuth, async (req, res, next) => {
                         include: [[sequelize.fn('MAX', sequelize.col('url')), 'previewImage']],
                         exclude: ['description', 'createdAt', 'updatedAt'],
                     },
-                    include: [{
-                        model: SpotImage,
-                        attributes: [ 'url' ]
-
-                    }]
+                    include: [
+                        {
+                            model: SpotImage,
+                            attributes: ['url'],
+                        },
+                    ],
                 },
             ],
-        })
+        });
 
-        
-        return res.json( currentBooking )
+        return res.json(currentBooking);
     } catch (error) {
         res.status(400).json({
-            message: error.message
-        })
+            message: error.message,
+        });
     }
-})
+});
 
 module.exports = router;
