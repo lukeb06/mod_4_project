18e967074d1f32e078054f4b2a409f3671fbc74a
Luke Barrier <lukebbarrier06@outlook.com>
2025-02-04 20:57:05 -0500

    Update

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator');
-const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
-const { handleValidationErrors } = require('../../utils/validation');
+const { requireAuth } = require('../../utils/auth');
 const { Booking, Spot, SpotImage, sequelize } = require('../../db/models');
 const router = express.Router();
 
