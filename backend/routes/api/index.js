0dbad77ccb9d863c4f22f4d71dbe5406b3941ae3
Katie Ha <katie.ha98@gmail.com>
2025-02-06 19:30:57 -0500

    delete image review

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,6 +5,7 @@ const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const bookingsRouter = require('./bookings.js');
 const reviewsRouter = require('./reviews.js');
+const reviewImagesRouter = require('./review-images.js');
 
 const { restoreUser } = require('../../utils/auth.js');
 
@@ -18,6 +19,7 @@ router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
 router.use('/bookings', bookingsRouter);
 router.use('/reviews', reviewsRouter);
+router.use('/review-images',reviewImagesRouter);
 
 router.post('/test', function (req, res) {
     res.json({ requestBody: req.body });
